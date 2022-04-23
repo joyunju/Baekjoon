48682ff5300ae43d7a8c97e2b1b16c7ab0873d51
joyunju <whdbswn1113@naver.com>
2022-04-23 22:52:13 +0900

    주어진 예제처럼 출력하는 문제 3

---
+++ src/com/backjoon/step1/Num_10171.java
@@ -0,0 +1,17 @@
+//문제 : 아래 예제와 같이 고양이를 출력하시오.
+package com.backjoon.step1;
+
+public class Num_10171 {
+
+	public static void main(String[] args) {
+		// \는 이스케이프 시퀀스로 사용하기 때문에 출력시 \를 하나 더 붙여야 함
+		// 이스케이프 시퀀스 : 프로그래밍 언어 특성상 표현할 수 없느 기능, 문자를 표현해준다.
+		// 컴퓨터를 제어한느 목적으로 사용되는 특수한 문자로 제어 시퀀스, 이스케이프 문자, 확장 비트열이라고도 한다.
+		System.out.println("\\    /\\");
+		System.out.println(")  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+
+	}
+
+}
