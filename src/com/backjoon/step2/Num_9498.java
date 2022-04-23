a9cc473d05b27c6287c97333fda5655e7dda8659
joyunju <whdbswn1113@naver.com>
2022-04-24 00:54:06 +0900

    시험 점수를 성적으로 바꾸는 문제

--- src/com/backjoon/step2/Num_9498.java
+++ src/com/backjoon/step2/Num_9498.java
@@ -1,12 +1,7 @@
-//두 수를 비교한 결과를 출력하는 문제
-//문제 : 두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오.
-//입력 : 첫째 줄에 A와 B가 주어진다. A와 B는 공백 한 칸으로 구분되어져 있다.
-//출력 : 첫째 줄에 다음 세 가지 중 하나를 출력한다.
-//A가 B보다 큰 경우에는 '>'를 출력한다.
-//A가 B보다 작은 경우에는 '<'를 출력한다.
-//A와 B가 같은 경우에는 '=='를 출력한다.
-//제한 : -10,000 ≤ A, B ≤ 10,000
-//예제 입력 : 1 2 --> 결과 값 : < 
+//문제 : 시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F를 출력하는 프로그램 작성
+//입력 : 첫째 줄에 시험 점수가 주어진다. 시험 점수는 0보다 크거나 같고, 100보다 작거나 같은 정수이다.
+//출력 : 시험 성적을 출력한다.
+//예제 입력 : 100 --> 예상 결과값 : A
 
 package com.backjoon.step2;
 
@@ -15,26 +10,25 @@ import java.util.Scanner;
 public class Num_9498 {
 
 	public static void main(String[] args) {
-		
-		//스캐너 생성
-		Scanner sc = new Scanner(System.in);
-		
-		//값 입력 받기 
-		int A = sc.nextInt();
-		int B = sc.nextInt();
-		
-		//if 조건문
-		if(A>B) {
-			System.out.println(">");
-		}else if(A<B) {
-			System.out.println("<");
-		}else {
-			System.out.println("==");
-		}
-		
-		
-		sc.close();
 
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		
+		//시험 점수에 따른 결과값 출력 조건문
+		
+		if (num <= 100 && num >= 90) {
+			System.out.print("A");
+		} else if (num >= 80 && num <= 89) {
+			System.out.print("B");
+		} else if (num >= 70 && num <= 79) {
+			System.out.print("C");
+		} else if (num >= 60 && num <= 69) {
+			System.out.print("D");
+		} else {
+			System.out.print("E");
+		}
+
+		sc.close();
 	}
 
 }
