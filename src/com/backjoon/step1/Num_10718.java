7639746590b040f40c5d2bcd9c33eec76d3c231c
joyunju <whdbswn1113@naver.com>
2022-04-23 22:42:01 +0900

    주어진 예제처럼 출력하는 문제 2

---
+++ src/com/backjoon/step1/Num_10718.java
@@ -0,0 +1,10 @@
+//문제 번호 : 10718
+//문제 내용 : 두 줄에 걸쳐 "강한친구 대한육군"을 한 줄에 한 번씩 출력한다.
+package com.backjoon.step1;
+
+public class Num_10718 {
+	public static void main(String[] args) {
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
+	}
+}
