e6c13d5745ab157659f045b234ba8ad3656fb7ac
joyunju <whdbswn1113@naver.com>
2022-04-23 22:55:30 +0900

    주어진 예제처럼 출력하는 문제 4

---
+++ src/com/backjoon/step1/Num_10172.java
@@ -0,0 +1,15 @@
+// 문제 : 아래 예제와 같이 개를 출력하시오.
+
+package com.backjoon.step1;
+
+public class Num_10172 {
+	public static void main(String[] args) {
+		// "를 출력하고 싶으면 \"로 작성하기
+		
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+	}
+}
